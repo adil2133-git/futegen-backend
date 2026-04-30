079d430706291f863e73acfa878be616615953bd
adil2133-git <adilaadi340@gmail.com>
2026-04-30 14:29:51 +0530

    cors changed

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://futgen-frontend-kpxajui7y-adils-projects-56610607.vercel.app",
     credentials: true
 }))
 
