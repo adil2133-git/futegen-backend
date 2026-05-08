b65507b31d92e3364316f7d9cfb1108c43c09f12
adil2133-git <adilaadi340@gmail.com>
2026-05-08 15:23:59 +0530

    mail fix

--- config/mail.js
+++ config/mail.js
@@ -6,10 +6,18 @@ const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true,
+
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+
+    connectionTimeout: 15000,
+    greetingTimeout: 15000,
+    socketTimeout: 15000,
+
+    debug: true,
+    logger: true
 });
 
 module.exports = transporter;
