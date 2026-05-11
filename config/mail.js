a887acf6194c8b15648cf31a4d44c48f338174de
adil2133-git <adilaadi340@gmail.com>
2026-05-11 11:25:49 +0530

    mail fix

--- config/mail.js
+++ config/mail.js
@@ -2,12 +2,10 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-    host: process.env.MAILTRAP_HOST,
-    port: Number(process.env.MAILTRAP_PORT),
-
+    service: "gmail",
     auth: {
-        user: process.env.MAILTRAP_USER,
-        pass: process.env.MAILTRAP_PASS
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
     }
 });
 
